5f52846e6f67170f3332b1ff22e897b70290a4c6
Akiyama <nhatvu148@gmail.com>
2021-03-10 22:59:00 +0900

    patch request

--- ParkyAPI/Controllers/NationalParksController.cs
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -79,5 +79,24 @@ namespace ParkyAPI.Controllers
 
             return CreatedAtRoute("GetNationalPark", new { nationalParkId = nationalParkObj.Id }, nationalParkObj);
         }
+
+        [HttpPatch("{nationalParkId:int}", Name = "UpdateNationalPark")]
+        public IActionResult UpdateNationalPark(int nationalParkId, [FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null || nationalParkId != nationalParkDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDto);
+
+            if (!_npRepo.UpdateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when updating the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+        }
     }
 }
